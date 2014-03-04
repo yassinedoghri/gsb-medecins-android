89920134ce8c78641a0e1607d6e81538c6a82d60
Doghri.yassine <doghri.yassine@gmail.com>
2014-03-04 11:57:23 +0000

    Possibilité de filtrer la liste des médecins + images

--- Medecins/src/y/doghri/medecins/MedAdapter.java
+++ Medecins/src/y/doghri/medecins/MedAdapter.java
@@ -10,7 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
+import java.util.ArrayList;
 import java.util.List;
 import model.Medecin;
 
@@ -18,39 +21,81 @@ import model.Medecin;
  *
  * @author ydoghri
  */
-public class MedAdapter extends BaseAdapter {
+public class MedAdapter extends BaseAdapter implements Filterable {
 
     private final List<Medecin> lesMeds;
+    private List<Medecin> filteredData;
     private final Context c;
 
     MedAdapter(List data, Context c) {
         lesMeds = data;
+        filteredData = data;
         this.c = c;
     }
+
     public int getCount() {
-        return lesMeds.size();
+        return filteredData.size();
     }
+
     public Object getItem(int i) {
-        return lesMeds.get(i);
+        return filteredData.get(i);
     }
+
     public long getItemId(int i) {
         return i;
     }
+
     public View getView(int i, View v, ViewGroup vg) {
         //création de la vue en analysant le xml ligneChev
-        LayoutInflater vi = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater vi = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         v = vi.inflate(R.layout.lignemed, null);
         TextView nom = (TextView) v.findViewById(R.id.nom);
         TextView prenom = (TextView) v.findViewById(R.id.prenom);
         TextView adresse = (TextView) v.findViewById(R.id.adresse);
         TextView specialite = (TextView) v.findViewById(R.id.specialite);
         TextView tel = (TextView) v.findViewById(R.id.tel);
-        Medecin leMed = lesMeds.get(i);
+        Medecin leMed = filteredData.get(i);
         nom.setText(leMed.getNom());
         prenom.setText(leMed.getPrenom());
         adresse.setText(leMed.getAdresse());
         specialite.setText(leMed.getSpecialite());
         tel.setText(leMed.getTel());
         return v;
+    }
+
+    public Filter getFilter() {
+        return new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults results = new FilterResults();
+
+                //If there's nothing to filter on, return the original data for your list
+                if (charSequence == null || charSequence.length() == 0) {
+                    results.values = lesMeds;
+                    results.count = lesMeds.size();
+                } else {
+                    List<Medecin> filterResultsData = new ArrayList<Medecin>();
+                    String q = charSequence.toString().toLowerCase();
+                    
+                    for (Medecin data : lesMeds) {
+                        if (data.getNom().toLowerCase().startsWith(q)) {
+                            filterResultsData.add(data);
+                        }
+                    }
+
+                    results.values = filterResultsData;
+                    results.count = filterResultsData.size();
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredData = (ArrayList<Medecin>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
--- Medecins/src/y/doghri/medecins/MedecinActivity.java
+++ Medecins/src/y/doghri/medecins/MedecinActivity.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package y.doghri.medecins;
 
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import java.util.List;
 import model.DAO;
 import model.Medecin;
@@ -17,15 +19,41 @@ import model.Medecin;
  *
  * @author ydoghri
  */
-public class MedecinActivity extends ListActivity{
+public class MedecinActivity extends ListActivity {
+
+    private EditText filterText = null;
+    MedAdapter adapter = null;
+
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.listemeds);
-        
+
         Intent inter = getIntent();
         String dep = inter.getStringExtra("leDep");
+
+        filterText = (EditText) findViewById(R.id.search_box);
+        filterText.addTextChangedListener(filterTextWatcher);
+
         List<Medecin> lesMeds = DAO.getLesMeds(dep);
-        MedAdapter adapter = new MedAdapter(lesMeds, this);
+        MedAdapter medAdap = new MedAdapter(lesMeds, this);
+        adapter = medAdap;
         setListAdapter(adapter);
     }
+
+    private final TextWatcher filterTextWatcher = new TextWatcher() {
+
+        public void afterTextChanged(Editable s) {
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before,
+                int count) {
+            adapter.getFilter().filter(s);
+        }
+
+    };
+
 }
